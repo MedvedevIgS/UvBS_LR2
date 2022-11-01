096567f81e61fe5090faaef42b2d3d000a194857
igorm <igormedvedev367@gmail.com>
2022-11-02 02:51:43 +0300

    Добавлен расчет и вывод неупорядоченного графа и упорядоченного графа

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from PyQt6 import uic, QtCore, QtWidgets, QtGui
 from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog
-from PyQt6.QtGui import QIntValidator, QDoubleValidator
+from PyQt6.QtGui import QIntValidator, QDoubleValidator, QPixmap
 import sys
+import os
 #import networkx as nx
 #import matplotlib.pyplot as plt
+import graphviz as gr
 
+
+def matrixTranspose(F):
+    F_T = [[row[i] for row in F] for i in range(len(F[0]))]
+    return F_T
+
+class resWindow(QMainWindow):
+    def __init__(self):
+        super(resWindow, self).__init__()
+        uic.loadUi("ResForm.ui", self)
+        self.Graf1.setPixmap(QPixmap('Graf1.png'))
+        self.Graf1.setScaledContents(True)
+        self.Graf2.setPixmap(QPixmap('Graf2.png'))
+        self.Graf2.setScaledContents(True)
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -12,21 +27,128 @@ class MainWindow(QMainWindow):
         self.kolV.setValidator(QIntValidator())
         self.MatrBut.clicked.connect(self.makeMatr)
         self.loadBut.clicked.connect(self.loadMatr)
+        self.ResBut.clicked.connect(self.resMatr)
+        self.AddBut.clicked.connect(self.addinMatr)
+        self.DelBut.clicked.connect(self.delinMatr)
         self.Error.setVisible(False)
         self.K=0
+        self.matrA_res=[]
+
+
+
+    def addinMatr(self):
+        if self.kolV.text()=='':
+            self.kolV.setText('1')
+        else:
+            self.kolV.setText(str(int(self.kolV.text())+1))
+        self.makeMatr()
+
+    def delinMatr(self):
+        ...
+
+
+
+    def resMatr(self):
+
+        self.matrA_res=[]
+        for i in range(self.K):
+            self.matrA_res.append([])
+            for j in range(self.K):
+                self.matrA_res[i].append(self.MatrA.cellWidget(i, j).text())
+        graf1='digraph G {\n'
+        for i in range(self.K):
+            for j in range(self.K):
+                if self.matrA_res[i][j]!='0' and self.matrA_res[i][j]!='-':
+                    graf1+=' '+str(i+1)+' -> '+str(j+1)+' [label="'+str(self.matrA_res[i][j])+'"];\n'
+        graf1+='}'
+        print(graf1)
+        uprgraf= []
+        for i in range(self.K):
+            uprgraf.append({'name': str(i+1), 'input': [], 'output': [], 'level': 0})
+        for i in range(self.K):
+            out=[]
+            for j in range(self.K):
+                if self.matrA_res[i][j] != '0' and self.matrA_res[i][j] != '-':
+                    out.append(j+1)
+            uprgraf[i]['output']=out
+
+        matrA_resT = matrixTranspose(self.matrA_res)
+
+        for i in range(self.K):
+            inp = []
+            for j in range(self.K):
+                if matrA_resT[i][j] != '0' and matrA_resT[i][j] != '-':
+                    inp.append(j+1)
+            uprgraf[i]['input'] = inp
+
+        initV=[]
+        level=0
+        iV=1
+        while iV <= self.K:
+            initVi=[]
+            for i in range(self.K):
+                if set(uprgraf[i]['input']).issubset(initV) and not (i+1 in initV):
+                    initVi.append(i+1)
+                    uprgraf[i]['name']=str(iV)+'('+uprgraf[i]['name']+')'
+                    uprgraf[i]['level']=level
+                    iV+=1
+            for i in range(len(initVi)):
+                initV.append(initVi[i])
+            level += 1
+
+        graf2 = 'digraph G {\n'
+        for i in range(self.K):
+            graf2+=' v'+str(i)+' [label = "'+uprgraf[i]['name']+'"];\n'
+
+        kolver=0
+        level=0
+        while kolver<len(uprgraf):
+            graf2 += ' subgraph cluster_level'+str(level)+' {\n node [style=filled]\n'
+            for i in range(self.K):
+                if uprgraf[i]['level']==level:
+                    graf2 += ' v'+str(i)+';'
+                    kolver+=1
+            graf2 += '\n label="Уровень '+str(level)+'";}\n'
+            level+=1
+
+
+        for i in range(self.K):
+            for j in range(self.K):
+                if self.matrA_res[i][j]!='0' and self.matrA_res[i][j]!='-':
+                    graf2+=' v'+str(i)+' -> v'+str(j)+' [label="'+str(self.matrA_res[i][j])+'"];\n'
+        graf2 += '}'
+        print(graf1)
+        os.remove('Graf1.png')
+        os.remove('Graf2.png')
+        grap1_view = gr.Source(graf1)
+        grap1_view.render(filename='Graf1', format='png')
+        grap2_view = gr.Source(graf2)
+        grap2_view.render(filename='Graf2', format='png')
+        os.remove('Graf1')
+        os.remove('Graf2')
+        global widget2
+        Res = resWindow()
+        widget2 = QtWidgets.QStackedWidget()
+        widget2.addWidget(Res)
+        widget2.setMinimumWidth(920)
+        widget2.setMinimumHeight(690)
+        widget2.show()
+        for i in uprgraf:
+            print(i)
+
 
     def loadMatr(self):
         fname = QFileDialog.getOpenFileName(self, 'Open file', 'Data\ ', '(*.txt)')
         f = open(fname[0], 'r')
         Data = f.read()
         f.close()
+        self.loadMatr_1(Data)
+
+    def loadMatr_1(self, Data):
         Data = Data.split('\n')
         for i in range(len(Data)):
             Data[i] = Data[i].split(' ')
-        for i in Data:
-            print(i)
         self.K=len(Data)
-        print(self.K)
         self.kolV.setText(str(self.K))
         self.MatrA.setRowCount(self.K)
         self.MatrA.setColumnCount(self.K)
@@ -85,7 +207,7 @@ def application():
     widget=QtWidgets.QStackedWidget()
     widget.addWidget(window)
     widget.setMinimumWidth(1050)
-    widget.setMinimumHeight(680)
+    widget.setMinimumHeight(720)
     widget.show()
     app.exec()
 
