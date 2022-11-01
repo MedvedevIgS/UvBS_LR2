4abee52d7a4d1f4c59bd30b612b5a1c0e965f99b
igorm <igormedvedev367@gmail.com>
2022-11-01 21:56:33 +0300

    Добавлена загрузки матрицы из txt файла

--- main.py
+++ main.py
@@ -10,11 +10,47 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         uic.loadUi("FormApp.ui", self)
         self.kolV.setValidator(QIntValidator())
-        self.MatrBut.clicked.connect(self.loadMatr)
+        self.MatrBut.clicked.connect(self.makeMatr)
+        self.loadBut.clicked.connect(self.loadMatr)
         self.Error.setVisible(False)
         self.K=0
 
     def loadMatr(self):
+        fname = QFileDialog.getOpenFileName(self, 'Open file', 'Data\ ', '(*.txt)')
+        f = open(fname[0], 'r')
+        Data = f.read()
+        f.close()
+        Data = Data.split('\n')
+        for i in range(len(Data)):
+            Data[i] = Data[i].split(' ')
+        for i in Data:
+            print(i)
+        self.K=len(Data)
+        print(self.K)
+        self.kolV.setText(str(self.K))
+        self.MatrA.setRowCount(self.K)
+        self.MatrA.setColumnCount(self.K)
+        h = 30 + 30 * self.K
+        w = 26 + 52 * self.K
+        self.MatrA.resize(QtCore.QSize(w, h))
+        for i in range(self.K):
+            self.MatrA.setColumnWidth(i, 1)
+        for i in range(int(self.MatrA.rowCount())):
+            for j in range(int(self.MatrA.columnCount())):
+                LineA = QtWidgets.QLineEdit()
+                if i == j:
+                    LineA.setText('0')
+                else:
+                    LineA.setText('-')
+                LineA.setValidator(QIntValidator())
+                self.MatrA.setCellWidget(i, j, LineA)
+        for i in range(self.K):
+            for j in range(self.K):
+                self.MatrA.cellWidget(i, j).setText(Data[i][j])
+
+
+
+    def makeMatr(self):
         if self.kolV.text() != '':
             if int(self.kolV.text()) > 0:
                 self.Error.setVisible(False)
@@ -34,7 +70,7 @@ class MainWindow(QMainWindow):
                             LineA.setText('0')
                         else:
                             LineA.setText('-')
-                        LineA.setValidator(QDoubleValidator())
+                        LineA.setValidator(QIntValidator())
                         self.MatrA.setCellWidget(i, j, LineA)
             else:
                 self.Error.setVisible(True)
